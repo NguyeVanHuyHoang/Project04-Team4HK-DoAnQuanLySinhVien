7a00ed022f722fffb97e3773ed19fd2424138e77
NguyeVanHuyHoang <89762941+NguyeVanHuyHoang@users.noreply.github.com>
2021-12-22 11:29:16 +0700

    Add project files.

---
+++ DoAnQuanLySinhVien/Frm_ThemSuaXoaLop.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoAnQuanLySinhVien
+{
+    public partial class Frm_ThemSuaXoaLop : Form
+    {
+        public Frm_ThemSuaXoaLop()
+        {
+            InitializeComponent();
+        }
+        public Form1 frm;
+        public delegate void _deDongTab();
+        public _deDongTab DongTab;
+        private void Frm_ThemSuaXoaLop_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_LopHoc_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_GVCN_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
