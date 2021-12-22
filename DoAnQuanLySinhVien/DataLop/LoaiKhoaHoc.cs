88a48f59ab6f0f60c5041c9aadb4c172d96b6e83
NguyeVanHuyHoang <89762941+NguyeVanHuyHoang@users.noreply.github.com>
2021-12-22 11:29:16 +0700

    Add project files.

---
+++ DoAnQuanLySinhVien/DataLop/LoaiKhoaHoc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAnQuanLySinhVien.DataLop
+{
+    class LoaiKhoaHoc
+    {
+        //Kiểu dữ liệu UserType
+        private int iDLoaiKhoaHoc;//ctrl+R+E--> Enter
+        private string tenLoaiKhoaHoc;
+
+        public int IDLoaiKhoaHoc { get => iDLoaiKhoaHoc; set => iDLoaiKhoaHoc = value; }
+        public string TenLoaiKhoaHoc { get => tenLoaiKhoaHoc; set => tenLoaiKhoaHoc = value; }
+    }
+}
